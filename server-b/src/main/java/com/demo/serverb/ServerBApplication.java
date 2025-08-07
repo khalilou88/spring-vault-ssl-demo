275258f801316f148e6e38b37c74381297895a49
khalilou88 <khalil.lagrida@gmail.com>
2025-08-07 11:00:46 +0200

    wip

--- server-b/src/main/java/com/demo/serverb/ServerBApplication.java
+++ server-b/src/main/java/com/demo/serverb/ServerBApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.demo.serverb", "com.demo.common"})
+@ComponentScan(basePackages = {"com.demo.serverb", "com.demo.common", "io.github.gridadev"})
 public class ServerBApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerBApplication.class, args);
