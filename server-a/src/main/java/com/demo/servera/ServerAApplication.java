2b07b0872bbb4eca3f9761fab80936c2041cd679
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-08-18 12:33:12 +0200

    update domain name

--- server-a/src/main/java/com/demo/servera/ServerAApplication.java
+++ server-a/src/main/java/com/demo/servera/ServerAApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.demo.servera", "com.demo.common", "io.github.gridadev"})
+@ComponentScan(basePackages = {"com.demo.servera", "com.demo.common", "com.gridatek"})
 public class ServerAApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerAApplication.class, args);
--- server-b/src/main/java/com/demo/serverb/ServerBApplication.java
+++ server-b/src/main/java/com/demo/serverb/ServerBApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.demo.serverb", "com.demo.common", "io.github.gridadev"})
+@ComponentScan(basePackages = {"com.demo.serverb", "com.demo.common", "com.gridatek"})
 public class ServerBApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerBApplication.class, args);
